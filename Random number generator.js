9e62c8fed80a3818b12645b21c669f43724354e1
Harry Lawal <harrylawal74@gmail.com>
2024-09-06 15:09:44 +0100

    Moving to Github
    moving my projects to github

---
+++ Random number generator.js
@@ -0,0 +1,13 @@
+//Random number 0 to 1
+document.getElementById("1").innerHTML= (Math.random())
+
+
+//Random number 0 to 10, whole numbers only
+document.getElementById("10").innerHTML = (Math.round(Math.random()*10))
+
+
+//Random number 0 to 100, whole numbers only
+document.getElementById("100").innerHTML = (Math.round(Math.random()*100))
+
+//Random number 50 to 100, whole numbers only
+document.getElementById("50-100").innerHTML = (Math.round(Math.random()*50)+50)
